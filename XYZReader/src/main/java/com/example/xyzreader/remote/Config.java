2cfe35b9fed822781f276ce9a3de6173f6ea03cb
Soojeong <soojeongshin2@gmail.com>
2018-10-03 22:02:34 +0900

    feat: Add log statement in Config

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -13,8 +13,9 @@ public class Config {
         URL url = null;
         try {
             url = new URL("https://go.udacity.com/xyz-reader-json" );
-        } catch (MalformedURLException ignored) {
+        } catch (MalformedURLException e) {
             // TODO: throw a real error
+            Log.e(TAG, "Problem building the URL.", e);
             Log.e(TAG, "Please check your internet connection.");
         }
 
