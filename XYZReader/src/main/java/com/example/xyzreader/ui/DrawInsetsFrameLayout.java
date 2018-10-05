b98f1f97d0a16fdb491968a35196d896a3b6938c
Soojeong <soojeongshin2@gmail.com>
2018-10-05 22:51:11 +0900

    refactor: Remove redundant modifier

--- XYZReader/src/main/java/com/example/xyzreader/ui/DrawInsetsFrameLayout.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DrawInsetsFrameLayout.java
@@ -153,7 +153,7 @@ public class DrawInsetsFrameLayout extends FrameLayout {
         }
     }
 
-    public static interface OnInsetsCallback {
-        public void onInsetsChanged(Rect insets);
+    public interface OnInsetsCallback {
+        void onInsetsChanged(Rect insets);
     }
 }
