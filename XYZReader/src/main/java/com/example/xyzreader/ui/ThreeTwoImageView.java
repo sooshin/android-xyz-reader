daba01dc8d903280c902dfa80447c3177795aac2
Soojeong <soojeongshin2@gmail.com>
2018-10-07 23:32:05 +0900

    feat: Create a ThreeTwoImageView

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ThreeTwoImageView.java
@@ -0,0 +1,47 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+/**
+ * The ThreeTwoImageView class is responsible for making ImageView 3:2 aspect ratio.
+ * This is used for photo image in the fragment_article_detail.xml.
+ */
+public class ThreeTwoImageView extends AppCompatImageView {
+
+    private static final int TWO = 2;
+    private static final int THREE = 3;
+
+    /**
+     * Creates ThreeTwoImageView
+     *
+     * @param context Used to talk to the UI and app resources
+     */
+    public ThreeTwoImageView(Context context) {
+        super(context);
+    }
+
+    public ThreeTwoImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ThreeTwoImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * This method measures the view and its content to determine the measured width and the measured
+     * height, which will make 3:2 aspect ratio.
+     *
+     * @param widthMeasureSpec horizontal space requirements as imposed by the parent
+     * @param heightMeasureSpec vertical space requirements as imposed by th parent
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int threeTwoHeight = MeasureSpec.getSize(widthMeasureSpec) * TWO / THREE;
+        int threeTwoHeightSpec =
+                MeasureSpec.makeMeasureSpec(threeTwoHeight, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, threeTwoHeightSpec);
+    }
+}
