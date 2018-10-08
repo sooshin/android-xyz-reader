6b54dc5e7fc65c98f9d109272fb3751a18da9c9e
Soojeong <soojeongshin2@gmail.com>
2018-10-09 00:00:04 +0900

    docs: Add documentation

--- XYZReader/src/main/java/com/example/xyzreader/pagetransformer/PopPageTransformer.java
+++ XYZReader/src/main/java/com/example/xyzreader/pagetransformer/PopPageTransformer.java
@@ -4,6 +4,11 @@ import android.support.annotation.NonNull;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
+/**
+ * A PopPageTransformer provides pop animation needed for transforming ViewPager scrolling.
+ *
+ * Reference: @see "https://github.com/dipanshukr/Viewpager-Transformation/wiki/Pop-Transformation"
+ */
 public class PopPageTransformer implements ViewPager.PageTransformer {
     @Override
     public void transformPage(@NonNull View view, float position) {
